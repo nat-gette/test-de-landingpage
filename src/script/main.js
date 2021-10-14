1fe02c21e842de292b4c5a7e7d4a149cea549aab
nat-gette <natalagette@gmail.com>
2021-10-14 19:37:58 +0500

    added general part of primary section, header and section about

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,5 @@
-$('.slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-});
+// $('.slider').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+// });
