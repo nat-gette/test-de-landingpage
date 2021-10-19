0326776031acfe871119c555349478a2bcdf2580
nat-gette <natalagette@gmail.com>
2021-10-19 19:23:44 +0500

    new sections changes. added modal pages

--- src/script/main.js
+++ src/script/main.js
@@ -3,3 +3,54 @@
 //     slidesToShow: 3,
 //     slidesToScroll: 3
 // });
+
+  
+// // let mainMenu = document.querySelector('.');
+// let mainHamburger = document.querySelector('.header__hamburger');
+
+// mainHamburger.addEventListener('click', () => {
+//     mainMenu.classList.toggle("header__list--active");
+// })
+
+// let menuLinks = document.querySelectorAll('.header__link');
+// for (let menuLink of menuLinks){
+//       menuLink.addEventListener('click', (e) => {
+//           e.preventDefault();
+//           let scrollToElem = menuLink.getAttribute('href');
+//           let coordinates = document.querySelector(scrollToElem).offsetTop;
+//           window.scrollTo({
+//               top:coordinates -150,
+//               behavior: 'smooth'
+//           })
+//       })
+// }
+
+let headerHunburger = document.querySelector('.header__hamburger');
+
+headerHunburger.addEventListener('click', () => {
+    headerHunburger.classList.toggle("header__hamburger--active");
+})
+
+let modalPage = document.querySelector('.modal');
+let questionBtn = document.querySelector('.question__btn')
+
+questionBtn.addEventListener('click', () => {
+    modalPage.classList.add("modal--active");
+})
+
+let modal = document.querySelector('.modal');
+ 
+modal.addEventListener('click', (e)  => {
+    if (e.target.className.includes('modal--active')) {
+        modalPage.classList.remove('modal--active');
+    }
+})
+
+let modalClose = document.querySelector('.modal__close');
+
+modalClose.addEventListener('click', () => {
+    modalPage.classList.remove('modal--active');
+})
+
+
+
