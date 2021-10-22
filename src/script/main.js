8f9424cb7f5ba0323e2dbb3430ddaa2541a2ea46
nat-gette <natalagette@gmail.com>
2021-10-22 18:09:01 +0500

    made popap

--- src/script/main.js
+++ src/script/main.js
@@ -1,116 +1,98 @@
+let headerHumburger = document.querySelector(".header__hamburger");
 
-let headerHumburger = document.querySelector('.header__hamburger');
+headerHumburger.addEventListener("click", () => {
+  headerHumburger.classList.toggle("header__hamburger--active");
+});
 
-headerHumburger.addEventListener('click', () => {
-    headerHumburger.classList.toggle("header__hamburger--active");
-})
+let modalPage = document.querySelector(".modal");
+let questionBtn = document.querySelector(".question__btn");
 
-let modalPage = document.querySelector('.modal');
-let questionBtn = document.querySelector('.question__btn')
+questionBtn.addEventListener("click", () => {
+  modalPage.classList.add("modal--active");
+});
 
-questionBtn.addEventListener('click', () => {
-    modalPage.classList.add("modal--active");
-})
+let modal = document.querySelector(".modal");
 
-let modal = document.querySelector('.modal');
- 
-modal.addEventListener('click', (e)  => {
-    if (e.target.className.includes('modal--active')) {
-        modalPage.classList.remove('modal--active');
-    }
-})
+modal.addEventListener("click", (e) => {
+  if (e.target.className.includes("modal--active")) {
+    modalPage.classList.remove("modal--active");
+  }
+});
 
-let modalClose = document.querySelector('.modal__close');
+let modalClose = document.querySelector(".modal__close");
 
-modalClose.addEventListener('click', () => {
-    modalPage.classList.remove('modal--active');
-})
+modalClose.addEventListener("click", () => {
+  modalPage.classList.remove("modal--active");
+});
 
-let hamburgerMenu = document.querySelector('.hamburger__menu');
+let hamburgerMenu = document.querySelector(".hamburger__menu");
 
-headerHumburger.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle("hamburger__menu--active");
-})
+headerHumburger.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("hamburger__menu--active");
+});
 
 let modalWindow = document.forms.modal;
 let userName = modalWindow.elements.userName;
 let userMail = modalWindow.elements.userMail;
 let userMessage = modalWindow.elements.userMessage;
-let modalButton = modalWindow.querySelector('.modal__button');
+let modalButton = modalWindow.querySelector(".modal__button");
+let inputPlace = modalWindow.querySelectorAll(".input__place");
 
-// modalWindow.addEventListener('submit', function (event) {
-//     event.preventDefault()
-//     console.log('clicked');
-//     console.log( 'userName: ', userName.value);
-//     console.log( 'userMale: ', userMail.value);
-//     console.log( 'userMessage: ', userMessage.value);
+modalWindow.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-// })
+  cleanErrors();
 
-let inputPlace = modalWindow.querySelectorAll('.input__place');
+  for (let i = 0; i < inputPlace.length; i++) {
+    if (!inputPlace[i].value) {
+      let error = document.createElement("div");
 
-// modalWindow.addEventListener('submit', function (event) {
-//     event.preventDefault()
-
-//     for (let i = 0; i < inputPlace.length; i++) {
-//         if (!inputPlace[i].value) {
-//             console.log('do not have any value ', inputPlace[i]);
-//             let error = document.createElement('div');
-//             error.className= 'error';
-//             error.style.color = 'red';
-//             error.innerHTML = 'Please fill out this field';
-//             inputPlace[i].parentElement.insertAdjacentElement('afterbegin', error) ;
-//         }
-//     }
-// })
-
-modalWindow.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    cleanErrors();
-    
-    for (let i = 0; i < inputPlace.length; i++) {
-        if (!inputPlace[i].value) {
-            // console.log('do not have any value ', inputPlace[i]);
-            let error = document.createElement('div');
-            error.className= 'error error-style';
-            inputPlace[i].classList.add('input-error');
-            error.innerHTML = 'Please fill out this field';
-            inputPlace[i].parentElement.insertAdjacentElement('beforeend', error) ;
-            modalWindow.classList.add('form-error');
-        }
-        else {
-            modalWindow.classList.remove('form-error');
-        }
+      error.className = "error error-style";
+      inputPlace[i].classList.add("input-error");
+      error.innerHTML = "Please fill out this field";
+      inputPlace[i].parentElement.insertAdjacentElement("beforeend", error);
+      modalWindow.classList.add("form-error");
+    } else {
+      modalWindow.classList.remove("form-error");
     }
+  }
 
-    let classError = [];
+  let classError = [];
 
-    inputPlace.forEach(item => {
-        if (item.classList.contains('input-error')) {
-            classError.push(item.className)
-        }
-    });
-
-    if (!classError.length) {
-        modalWindow.classList.remove('form-error');
+  inputPlace.forEach((item) => {
+    if (item.classList.contains("input-error")) {
+      classError.push(item.className);
     }
-    
-    if (!modalWindow.classList.contains('form-error')) {
-        console.log('form send');
-        modalWindow.reset();
-    }
+  });
 
-})
+  if (!classError.length) {
+    modalWindow.classList.remove("form-error");
+  }
+
+  if (!modalWindow.classList.contains("form-error")) {
+    // console.log('form send');
+    modalWindow.reset();
+
+    let success = document.createElement("div");
+    success.className = "success success-style";
+    success.innerHTML = "Your message successfully sent";
+    userMessage.parentElement.insertAdjacentElement("beforeend", success);
+
+    setTimeout(() => {
+      success.classList.remove("success-style");
+      success.classList.remove("success");
+      success.innerHTML = "";
+    }, 3000);
+  }
+});
 
 let cleanErrors = function () {
-    let errors = modalWindow.querySelectorAll('.error');
+  let errors = modalWindow.querySelectorAll(".error");
 
-    for (let i = 0; i < errors.length; i++) {
-        errors[i].remove();
-    }
-    for (let i = 0; i < inputPlace.length; i++) {
-        inputPlace[i].classList.remove('input-error');
-    }
-}
-
+  for (let i = 0; i < errors.length; i++) {
+    errors[i].remove();
+  }
+  for (let i = 0; i < inputPlace.length; i++) {
+    inputPlace[i].classList.remove("input-error");
+  }
+};
